eff52664fe43191b8a634eebdb97c20c5c9ed16f
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-09-10 14:53:20 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Friendsrate2.0',
   css: {
     postcss: {
       plugins: [
