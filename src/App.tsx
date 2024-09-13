3a803e31ce1e4f15f7554ad4074df0f7c365fc2a
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-09-13 15:06:51 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { useAuth } from "./redux/Hooks/useAuth";
@@ -25,32 +25,46 @@ export const App = () => {
     <b>Refreshing user...</b>
   ) : (
     <>
-<Routes>
-<Route path="/" element={<PrivateRoute redirectTo="/signin" element={<SharedLayout/>} />}>
-
-<Route path="/mainpage" element={
-        <PrivateRoute redirectTo="/signin" element={<MainPage/>} />
-      }/> 
- <Route path="/profile" element={
-        <PrivateRoute redirectTo="/signin" element={<ProfilePage/>} />
-      }/> 
-       <Route path="/settings" element={
-        <PrivateRoute redirectTo="/signin" element={<SettingsPage/>} />
-      }/> 
-       <Route path="/rating" element={
-        <PrivateRoute redirectTo="/signin" element={<RatingPage/>} />
-      }/> 
-
-</Route>
-<Route
-      path="/signin"
-      index element={
-        <RestrictedRoute redirectTo="/" element={<LogPage/>} />
-      }
-    />
-</Routes> 
-</>
-  )
-    }
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute redirectTo="/signin" element={<SharedLayout />} />
+          }
+        >
+          <Route
+            path="/mainpage"
+            element={
+              <PrivateRoute redirectTo="/signin" element={<MainPage />} />
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute redirectTo="/signin" element={<ProfilePage />} />
+            }
+          />
+          <Route
+            path="/settings"
+            element={
+              <PrivateRoute redirectTo="/signin" element={<SettingsPage />} />
+            }
+          />
+          <Route
+            path="/rating"
+            element={
+              <PrivateRoute redirectTo="/signin" element={<RatingPage />} />
+            }
+          />
+        </Route>
+        <Route
+          path="/signin"
+          index
+          element={<RestrictedRoute redirectTo="/" element={<LogPage />} />}
+        />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
