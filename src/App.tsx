592434676842a92c7edb194ef571ed9a56eae7ed
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-09-13 12:04:12 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,76 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import "./App.css";
-import { useAuth } from "./redux/Hooks/useAuth";
 import { useEffect } from "react";
+import { useAuth } from "./redux/Hooks/useAuth";
 import { refreshUser } from "./redux/Auth/operations";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { RestrictedRoute } from "./RestrictedRoute";
 import { PrivateRoute } from "./PrivateRoute";
-import { LogPage } from "./pages/LogPage.js";
-import { MainPage } from "./pages/MainPage.js";
+import { RestrictedRoute } from "./RestrictedRoute";
+import { Dispatch } from "./redux/store.js";
 import SharedLayout from "./components/SharedLayout/SharedLayout.js";
-
+import { MainPage } from "./pages/MainPage.js";
+import ProfilePage from "./pages/ProfilePage.js";
 import { SettingsPage } from "./pages/SettingsPage.js";
 import { RatingPage } from "./pages/RatingPage.js";
-import { Dispatch } from "./redux/store.js";
+import { LogPage } from "./pages/LogPage.js";
 import { NotFoundPage } from "./pages/NotFoundPage.js";
-import ProfilePage from "./pages/ProfilePage.js";
 
 export const App = () => {
   const dispatch: Dispatch = useDispatch();
-  const { isLoggedIn, isRefreshing } = useAuth();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return isRefreshing ? (
-    <b>Refreshing user...</b>
-  ) : (
+  if (isRefreshing) {
+    return <b>Refreshing user...</b>;
+  }
 
-    <Routes>
-    <Route path="/" element={<SharedLayout />}>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <SharedLayout />, 
+      children: [
+        {
+          path: "mainpage",
+          element: (
+            <PrivateRoute redirectTo="/login" component={<MainPage />} />
+          ),
+        },
+        {
+          path: "profile",
+          element: (
+            <PrivateRoute redirectTo="/login" component={<ProfilePage />} />
+          ),
+        },
+        {
+          path: "settings",
+          element: (
+            <PrivateRoute redirectTo="/login" component={<SettingsPage />} />
+          ),
+        },
+        {
+          path: "rating",
+          element: (
+            <PrivateRoute redirectTo="/login" component={<RatingPage />} />
+          ),
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: (
+        <RestrictedRoute redirectTo="/mainpage" component={<LogPage />} />
+      ),
+    },
+    {
+      path: "*", 
+      element: <NotFoundPage />,
+    },
+  ]);
 
-    {/* Public routes */}
-    <Route
-      path="/login"
-      element={<RestrictedRoute redirectTo="/mainpage" component={LogPage} />}
-    />
 
-    {/* Private routes */}
-
-      <Route
-        path="mainpage"
-        element={<PrivateRoute redirectTo="/login" component={MainPage} />}
-      />
-      <Route
-        path="profile"
-        element={<PrivateRoute redirectTo="/login" component={ProfilePage} />}
-      />
-      <Route
-        path="settings"
-        element={<PrivateRoute redirectTo="/login" component={SettingsPage} />}
-      />
-      <Route
-        path="rating"
-        element={<PrivateRoute redirectTo="/login" component={RatingPage} />}
-      />
-    </Route>
-
-    {/* Redirect non-logged users to the login page by default */}
-    <Route path="/" element={isLoggedIn ? <Navigate to="/mainpage" /> : <Navigate to="/login" />} />
-
-    {/* 404 Page */}
-    <Route path="*" element={<NotFoundPage />} />
-  </Routes>
-);
+  return <RouterProvider router={router} />;
 };
-
 
 export default App;
