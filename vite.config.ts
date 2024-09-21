4629bb854dbb5dca0db3abb4f930f38948d59229
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-09-21 10:05:07 +0200

    Added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Friendsrate2.0/',
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-      ],
-    },
-  },
-})
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
